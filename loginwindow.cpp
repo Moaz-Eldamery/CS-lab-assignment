59abdc1e58a71f05a60dc27d66d876eb6b8593c3
John Hany <johnhanyaucegypt.edu>
2024-03-03 21:10:38 +0200

    Login Button

--- loginwindow.cpp
+++ loginwindow.cpp
@@ -1,14 +1,34 @@
 #include "loginwindow.h"
-#include "ui_loginwindow.h"
+#include "ui_loginwindow.h"\
+
 
 LoginWindow::LoginWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::LoginWindow)
 {
     ui->setupUi(this);
+
 }
 
 LoginWindow::~LoginWindow()
 {
     delete ui;
 }
+
+void LoginWindow::on_Login_clicked()
+{
+
+
+
+}
+
+
+void LoginWindow::on_Register_clicked()
+{
+
+    a[i].username=ui->username->text();
+    a[i].password=ui->password->text();
+
+
+}
+
--- loginwindow.h
+++ loginwindow.h
@@ -17,7 +17,21 @@ public:
     LoginWindow(QWidget *parent = nullptr);
     ~LoginWindow();
 
+private slots:
+    void on_Login_clicked();
+
+    void on_Register_clicked();
+
 private:
     Ui::LoginWindow *ui;
+    struct customer
+    {
+        QString username;
+        QString password;
+    };
+    customer *a = new customer[100];
+
+
+    int i=0;
 };
 #endif // LOGINWINDOW_H
